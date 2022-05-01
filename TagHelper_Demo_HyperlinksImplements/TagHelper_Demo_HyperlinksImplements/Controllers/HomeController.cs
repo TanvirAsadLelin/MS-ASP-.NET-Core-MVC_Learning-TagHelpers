ce8a187758a035fd7a23903b1ddbe8be66982252
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-01 17:42:29 +0600

    Tag helper theory with Implements Hyperlink concept

---
+++ TagHelper_Demo_HyperlinksImplements/TagHelper_Demo_HyperlinksImplements/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TagHelper_Demo_HyperlinksImplements.Controllers
+{   
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {   
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("[action]/{id}")]
+        public int Details(int id)
+        {
+            return id;
+        }
+    }
+}
