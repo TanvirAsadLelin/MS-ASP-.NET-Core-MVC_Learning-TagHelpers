6eefc73719afb3eda21fc344cef32421cb46167e
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-03 01:21:00 +0600

    Tag helper Benefit concept with example added

---
+++ BenefitTagHelper_Demo/BenefitTagHelper_Demo/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BenefitTagHelper_Demo.Controllers
+{   [Route("Lelin/[controller]")]
+    public class HomeController : Controller
+    {    
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
